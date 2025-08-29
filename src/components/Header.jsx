67e115ebc8c2b2d4d330a1e1ca8a833caeb65909
Varun Harinath <varunharinath15@gmail.com>
2025-08-29 08:17:49 -0500

    Updated MovieGrid.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles.css";
-import logo from "../../public/assets/logo.png";
+import logo from "../../public/images/logo.png";
 
 const Header = () => {
   return (
--- src/components/Moviegrid.jsx
+++ src/components/Moviegrid.jsx
@@ -1,21 +1,52 @@
+// // import React from "react";
+// // import "../styles.css";
+
+// // const Moviegrid = () => {
+// //     const age =10;
+// //   return (
+// //     <>
+// //     <div>
+// //         {age}
+// //     </div>
+// //     </>
+// //   )
+// // };
+
+// // export default Moviegrid;
+
 // import React from "react";
+// import { useState, useEffect } from "react";
 // import "../styles.css";
 
 // const Moviegrid = () => {
-//     const age =10;
+//   const [movies, setMovies] = useState([]);
+
+//   useEffect(() => {
+//     fetch("movies.json")
+//       .then((response) => response.json())
+//       .then((data) => setMovies(data));
+//   }, []);
 //   return (
 //     <>
-//     <div>
-//         {age}
-//     </div>
+//       <div className="movies-grid">
+//         {movies.map((movie) => {
+//           <div key={movie.id} className="movie-card">
+//             <img src={`images/${movie.image}`} alt={movie.title} />
+//             <div className="movie-card-info">
+//               <h3 className="movie-card-title">{movie.title}</h3>
+//               <p className="movie-card-genre">{movie.genre}</p>
+//               <p className="movie-card-rating">{movie.rating}</p>
+//             </div>
+//           </div>;
+//         })}
+//       </div>
 //     </>
-//   )
+//   );
 // };
 
 // export default Moviegrid;
-
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import "../styles.css";
 
 const Moviegrid = () => {
   const [movies, setMovies] = useState([]);
@@ -24,11 +55,21 @@ const Moviegrid = () => {
     fetch("movies.json")
       .then((response) => response.json())
       .then((data) => setMovies(data));
-  });
+  }, []);
+
   return (
     <>
-      <div>
-        <h1>{movies.length}</h1>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <div key={movie.id} className="movie-card">
+            <img src={`images/${movie.image}`} alt={movie.title} />
+            <div className="movie-card-info">
+              <h3 className="movie-card-title">{movie.title}</h3>
+              <p className="movie-card-genre">{movie.genre}</p>
+              <p className="movie-card-rating">{movie.rating}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
