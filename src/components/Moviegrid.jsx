d36ce35a757d455067a6b4633caf691d3a03da43
Varun Harinath <varunharinath15@gmail.com>
2025-09-03 08:43:27 -0500

    Update to the MovieGrid

--- src/components/Moviegrid.jsx
+++ src/components/Moviegrid.jsx
@@ -1,56 +1,10 @@
-// // import React from "react";
-// // import "../styles.css";
-
-// // const Moviegrid = () => {
-// //     const age =10;
-// //   return (
-// //     <>
-// //     <div>
-// //         {age}
-// //     </div>
-// //     </>
-// //   )
-// // };
-
-// // export default Moviegrid;
-
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import "../styles.css";
-
-// const Moviegrid = () => {
-//   const [movies, setMovies] = useState([]);
-
-//   useEffect(() => {
-//     fetch("movies.json")
-//       .then((response) => response.json())
-//       .then((data) => setMovies(data));
-//   }, []);
-//   return (
-//     <>
-//       <div className="movies-grid">
-//         {movies.map((movie) => {
-//           <div key={movie.id} className="movie-card">
-//             <img src={`images/${movie.image}`} alt={movie.title} />
-//             <div className="movie-card-info">
-//               <h3 className="movie-card-title">{movie.title}</h3>
-//               <p className="movie-card-genre">{movie.genre}</p>
-//               <p className="movie-card-rating">{movie.rating}</p>
-//             </div>
-//           </div>;
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Moviegrid;
 import React, { useState, useEffect } from "react";
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
 const Moviegrid = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("movies.json")
@@ -58,14 +12,26 @@ const Moviegrid = () => {
       .then((data) => setMovies(data));
   }, []);
 
+  const filterChange = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <>
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Movies"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
       <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} />
+        {filterChange.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
